d195b43183deaf94fc903673612720fd507cb56a
Max <sonthaya.nt@hotmail.com>
2020-10-04 02:38:19 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,5 +1,5 @@
 Blockly.Python['pms7003_read'] = function(block) {
-  Blockly.Python.definitions_['import_tm1637'] = 'import pms7003';
+  Blockly.Python.definitions_['import_pms7003'] = 'import pms7003';
 
   var dropdown_clk = block.getFieldValue('pin');
   var dropdown_type = block.getFieldValue('type');
