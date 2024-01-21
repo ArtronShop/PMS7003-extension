3e2dc6865c82f4a82459e24f792784f18d700b4c
ArmCool <157279027+ArmCool@users.noreply.github.com>
2024-01-21 22:45:54 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,9 +1,9 @@
-Blockly.Python['pms7003_read'] = function(block) {
-  Blockly.Python.definitions_['import_pms7003'] = 'import pms7003';
+Blockly.Python['pms3003_read'] = function(block) {
+  Blockly.Python.definitions_['import_pms3003'] = 'import pms7003';
 
   var dropdown_clk = block.getFieldValue('pin');
   var dropdown_type = block.getFieldValue('type');
 
-  var code = `pms7003.read(${dropdown_clk})[${dropdown_type}]`;
+  var code = `pms3003.read(${dropdown_clk})[${dropdown_type}]`;
   return [code, Blockly.Python.ORDER_NONE];
 };
